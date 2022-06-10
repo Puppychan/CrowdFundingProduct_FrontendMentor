16b09b0f174ec9dbb924548d800fd3d939af0bdd
Matsuri <nhungmaitran1412@gmail.com>
2022-06-10 19:53:35 +0700

    done 1 card

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 let menuIcon = document.querySelector(".header-menuicon");
-let header = document.querySelector('.header')
+let header = document.querySelector('.header');
+let bookmarkButton = document.querySelector(".main-intro-buttons-bookmark");
+
+// open side menu on mobile
 menuIcon.addEventListener("click", event => {
     header.classList.toggle("open");
     menuIcon.setAttribute("src", menuIcon.getAttribute("src").includes("hamburger")? "./images/icon-close-menu.svg" : "./images/icon-hamburger.svg");
 });
+
+// click bookmark button
+bookmarkButton.addEventListener('click', event => {
+    bookmarkButton.classList.toggle("tick");
+    bookmarkButton.querySelector("span").innerHTML = bookmarkButton.classList.contains("tick")? "Bookmarked" : "Bookmark";
+})
